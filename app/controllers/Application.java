5fd5507bb0e18ccb0de927183d862f618d9e7dd3
ioanas <ioana.sebestyen@endava.com>
2012-10-13 15:31:23 +0300

    Added persistence settings.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import model.User;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+	@Transactional
+	public static Result index() {
+		User user = JPA.em().find(User.class, 1L);
+		String message = null;
+		if (user == null) {
+			message = "No user in DB with id = 1.";
+		} else {
+			message = "User in DB with id = 1: " + user.getUsername();
+		}
+		return ok(index.render(message));
+	}
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+       "mysql" % "mysql-connector-java" % "5.1.19",
+       "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
