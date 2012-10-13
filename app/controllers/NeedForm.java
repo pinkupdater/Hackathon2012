3f28d6bc642107cd22a88e50ef6ff264b4d30f8c
adruhora <adruhora@EN62358.endava.net>
2012-10-13 21:01:01 +0300

    local commit

--- app/controllers/NeedForm.java
+++ app/controllers/NeedForm.java
@@ -1,13 +1,30 @@
 package controllers;
 
+import models.NeedItem;
+import play.data.Form;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.need.formNeed;
 
 public class NeedForm extends Controller{
 	
-	public static Result formNeed(){
-		return ok(formNeed.render("salut"));
+	final static Form<NeedItem> needForm = form(NeedItem.class);
+	
+	public static Result blank() {
+        return ok(formNeed.render(needForm));
+    }
+	
+	@Transactional
+	public static Result submit() {
+		 Form<NeedItem> filledForm = needForm.bindFromRequest();
+		 NeedItem created = filledForm.get();
+		 created.save();
+		 return null;
 	}
+	
+	/*public static Result formNeed(){
+		return ok(formNeed.render("salut"));
+	}*/
 
 }
