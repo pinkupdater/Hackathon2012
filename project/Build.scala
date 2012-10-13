87cf0771f21dcab6906cab6f2be10a3385616f38
ioanas <ioana.sebestyen@endava.com>
2012-10-13 17:39:49 +0300

    Removed ebean support - it messed up with jpa configs.

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,8 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here  
+       ebeanEnabled := false    
     )
 
 }
